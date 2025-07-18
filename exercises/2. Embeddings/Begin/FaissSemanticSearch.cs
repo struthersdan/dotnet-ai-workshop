9c6474e5d05d5f83c613108c6fe5a0611cc6a476
Daniel Struthers <daniel.struthers@iqmetrix.com>
2025-07-18 12:26:58 -0700

    module 3 done

--- exercises/2. Embeddings/Begin/FaissSemanticSearch.cs
+++ exercises/2. Embeddings/Begin/FaissSemanticSearch.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.AI;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.AI;
 
 namespace Embeddings;
 
@@ -14,12 +15,61 @@ public class FaissSemanticSearch
     // Runs in process on CPU using a small embedding model
     // Alternatively use OllamaEmbeddingGenerator or OpenAIEmbeddingGenerator
     private IEmbeddingGenerator<string, Embedding<float>> EmbeddingGenerator { get; } =
-        new LocalEmbeddingsGenerator(); 
+        new LocalEmbeddingsGenerator();
 
     public async Task RunAsync()
     {
         var githubIssues = TestData.GitHubIssues.TakeLast(TestDataSetSize).ToDictionary(x => x.Number, x => x);
 
+        var index = await LoadOrCreateIndexAsync("index_hnsw.bin", githubIssues);
+
+
         // TODO: Build an index using FAISS
+        while (true)
+        {
+            Console.Write("\nQuery: ");
+            var input = Console.ReadLine()!;
+            if (input == "") break;
+
+            var inputEmbedding = await EmbeddingGenerator.GenerateVectorAsync(input);
+            var sw = new Stopwatch();
+            sw.Start();
+            var (resultDistances, resultIds) = index.SearchFlat(1, inputEmbedding.ToArray(), 3);
+            sw.Stop();
+            for (var i = 0; i < resultDistances.Length; i++)
+            {
+                var distance = resultDistances[i];
+                var id = (int)resultIds[i];
+                Console.WriteLine($"({distance:F2}): {githubIssues[id].Title}");
+            }
+            Console.WriteLine($"Search duration: {sw.ElapsedMilliseconds:F2}ms");
+        }
+    }
+
+    private async Task<FaissNet.Index> LoadOrCreateIndexAsync(string filename, IDictionary<int, GitHubIssue> data)
+    {
+        if (File.Exists(filename))
+        {
+            var result = FaissNet.Index.Load(filename);
+            Console.WriteLine($"Loaded index with {result.Count} entries");
+            return result;
+        }
+
+        var index = FaissNet.Index.Create(EmbeddingDimension, "IDMap2,HNSW32", FaissNet.MetricType.METRIC_INNER_PRODUCT);
+
+        foreach (var issuesChunk in data.Chunk(1000))
+        {
+            Console.Write($"Embedding issues: {issuesChunk.First().Key} - {issuesChunk.Last().Key}");
+            var embeddings = await EmbeddingGenerator.GenerateAsync(issuesChunk.Select(i => i.Value.Title));
+
+            Console.WriteLine(" Inserting into index...");
+            index.AddWithIds(
+                embeddings.Select(e => e.Vector.ToArray()).ToArray(),
+                issuesChunk.Select(i => (long)i.Key).ToArray());
+        }
+
+        index.Save(filename);
+        return index;
+
     }
 }
--- exercises/2. Embeddings/Begin/Program.cs
+++ exercises/2. Embeddings/Begin/Program.cs
@@ -3,5 +3,5 @@
 // Toggle which of these are commented out to run the corresponding code
 
 //await new SentenceSimilarity().RunAsync();
-await new ManualSemanticSearch().RunAsync();
-//await new FaissSemanticSearch().RunAsync();
+//await new ManualSemanticSearch().RunAsync();
+await new FaissSemanticSearch().RunAsync();
